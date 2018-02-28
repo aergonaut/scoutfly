d5d4fdba25433f0b9a5e597b67b2e1663a449ee1
Chris Fung <aergonaut@gmail.com>
2018-02-27 18:19:58 -0800

    tweaks to preloading

--- App.js
+++ App.js
@@ -1,7 +1,9 @@
 import React from "react";
 import { StackNavigator } from "react-navigation";
+import { AppLoading, Asset } from "expo";
 import MonsterFieldGuide from "./src/MonsterFieldGuide";
 import MonsterDetails from "./src/MonsterDetails";
+import Monsters from "./src/monsters";
 
 const RootStack = StackNavigator({
   MonsterFieldGuide: {
@@ -12,8 +14,32 @@ const RootStack = StackNavigator({
   }
 });
 
+function cacheImages() {
+  return Monsters.Large.map(({ icon }) => {
+    return Asset.fromModule(icon).downloadAsync();
+  });
+}
+
 export default class App extends React.Component {
+  state = {
+    isReady: false
+  };
+
+  async loadAssets() {
+    const images = cacheImages();
+    await Promise.all([...images]);
+  }
+
   render() {
-    return <RootStack />;
+    if (!this.state.isReady) {
+      return (
+        <AppLoading
+          startAsync={this.loadAssets}
+          onFinish={() => this.setState({ isReady: true })}
+        />
+      );
+    } else {
+      return <RootStack />;
+    }
   }
 }
